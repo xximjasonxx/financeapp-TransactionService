1b66eb7036df084373a6284acdde68ea720a11c2
Jason Farrell <imjason@gmail.com>
2018-09-16 09:25:53 -0500

    Added Transaction audit write

--- Services/TransService.cs
+++ Services/TransService.cs
@@ -1,14 +1,25 @@
-
+using System;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 using TransactionService.Models;
 
 namespace TransactionService.Services
 {
     public static class TransService
     {
+        static IMongoCollection<Transaction> GetCollection()
+        {
+            var client = new MongoClient(Environment.GetEnvironmentVariable("MongoConnectionString", EnvironmentVariableTarget.Process));
+            var database = client.GetDatabase("transactions");
+            return database.GetCollection<Transaction>("transactions");
+        }
+
         public static async Task<string> WriteNewTransaction(Transaction transaction)
         {
-            return string.Empty;
+            var collection = GetCollection();
+            await collection.InsertOneAsync(transaction);
+
+            return transaction.Id;
         }
     }
 }
